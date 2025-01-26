8f6890cb1e64db9fdbf32674df7e9f009cf71fb5
hsuan97330 <84563435+hsuan97330@users.noreply.github.com>
2025-01-26 20:37:20 +0800

    pretest-far3 url.js

--- url.js
+++ url.js
@@ -10,7 +10,7 @@
 const urls = [
 	'https://zh.surveymonkey.com/r/BWLM3V2',/*遠1*/
 	'https://zh.surveymonkey.com/r/NTS5JVJ',/*遠2*/
-	'https://zh.surveymonkey.com/r/NTTYVZR',/*遠3*/
+	/*'https://zh.surveymonkey.com/r/NTTYVZR',遠3*/
 	'https://zh.surveymonkey.com/r/BWDMRHV',/*近1*/
 	'https://zh.surveymonkey.com/r/BSMGWQK',/*近2*/
 	'https://zh.surveymonkey.com/r/NTGBR2X',/*近3*/
